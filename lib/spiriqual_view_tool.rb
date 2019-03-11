9cd181de5f08f16331d7f88d3c44df68923b39c9
Jakub Vintera <jakubvintera@MacBook-Pro.local>
2019-03-11 18:00:47 +0100

    Dev tool initial commit

---
+++ lib/spiriqual_view_tool.rb
@@ -0,0 +1,7 @@
+require "spiriqual_view_tool/version"
+require "spiriqual_view_tool/renderer"
+
+module SpiriqualViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/spiriqual_view_tool/version.rb
@@ -0,0 +1,3 @@
+module SpiriqualViewTool
+  VERSION = "0.1.0"
+end
